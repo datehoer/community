4f8199ad1dfd84b97d76db7d4ebf9db1a271943c
humingkai <humingkai@int-yt.com>
2022-11-01 17:40:04 +0800

    add entry mod

---
+++ models/entry.js
@@ -0,0 +1,36 @@
+const redis = require('redis');
+// 创建redis客户端实例
+const db = redis.createClient({url: "redis://10.20.0.22:9379/13"});
+
+class Entry {
+    constructor(obj){
+        // 循环遍历传入对象中的键
+        for (let key in obj){
+            // 合并值
+            this[key] = obj[key];
+        }
+    }
+    save(cb){
+        // 将保存的消息转换为JSON字符串
+        const entryJSON = JSON.stringify(this);
+        // 将JSON字符串保存到redis列表中
+        db.lPush('entries', entryJSON, (err)=>{
+            if(err) return cb(err);
+            cb();
+        });
+    }
+    static getRange(from, to, cb){
+        // 获取消息记录的redis lRange 函数
+        db.lRange('entries', from, to, (err, items)=>{
+            if(err) return cb(err);
+            let entries = [];
+            items.forEach((item) => {
+                // 解码之前保存为JSON的消息记录
+                entries.push(JSON.parse(item))
+            });
+            cb(null, entries);
+        })
+    }
+}
+
+module.exports = Entry;
