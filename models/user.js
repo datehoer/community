ad4806e215bce13435e6d86f22203395d150fb5d
humingkai <humingkai@int-yt.com>
2022-11-02 14:56:10 +0800

    some change

--- models/user.js
+++ models/user.js
@@ -2,11 +2,55 @@ const redis = require('redis');
 const bcrypt = require('bcrypt');
 // 创建redis客户端实例
 const db = redis.createClient({url: "redis://10.20.0.22:9379/14"});
+db.connect()
+async function redisCommand(){
+
+}
+async function bcryptHash(pass, salt){
+    // 生成哈希
+    let result = await bcrypt.hash(pass, salt)
+    return new Promise((resolve, reject)=>{
+        resolve(result)
+    })
+}
+async function genSalt(){
+    // 生成有12个字符的盐
+    let sale = await bcrypt.genSalt(12)
+    return new Promise((resolve, reject)=>{
+        resolve(sale)
+    })
+}
 class User{
     constructor(obj){
         for(let key in obj){
             this[key] = obj[key]
         }
+    }
+    static getByName(name){
+        User.getId(name)
+    }
+    static getId(name){
+        db.get(`user:id:${name}`)
+            .then(res=>{
+                User.get(res)
+            })
+    }
+    static get(id){
+        db.hGetAll(`user:${id}`)
+            .then(res => {
+                new User(res)
+            })
+    }
+    static authenticate(name, pass, cb){
+        User.getByName(name, (err, user)=>{
+            if(err) return cb(err);
+            if(!user.id) return cb();
+            bcryptHash(pass, user.salt, (err, hash)=>{
+                if(err) return cb(err);
+                if(hash == user.pass) return cb(null, user)
+                cb()
+            })
+        })
     }
     save(cb){
         // 如果设置了id则表示用户已存在
@@ -51,18 +95,25 @@ class User{
             })
     }
     hashPassword(cb){
-        // 生成有12个字符的盐
-        let salt = bcrypt.genSalt(12);
-        this.salt = salt;
-        // 生成哈希
-        let result = bcrypt.hash(this.pass, salt)
-        this.pass = result;
-        cb();
+        genSalt()
+            .then(res=>{
+                bcryptHash(this.pass, res)
+                    .then(result=>{
+                        this.pass = result
+                        cb();
+                    })
+            })
     }
 }
 
-const user = new User({name: 'Example', pass: 'tes1t'});
-user.save((err)=>{
-    if(err) return console.error(err);
-    console.log('user id %d', user.id);
-})
+// const user = new User({name: 'Example', pass: 'tes1t'});
+
+// user.save((err)=>{
+//     if(err) return console.error(err);
+//     console.log('user id %d', user.id);
+// })
+
+User.getByName('Example')
+
+
+db.quit()
